853ba0929d9a059c92477e8dc888439b167e2af9
takaki <takaki55730317@gmail.com>
2024-06-20 02:54:25 +0900

    add welcome page setup tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
-import { defineNuxtConfig } from 'nuxt/config';
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  css: ["@/assets/css/tailwind.css"],
   devtools: { enabled: true },
   plugins: ["~/plugins/axios.ts"],
+  modules: ["@nuxtjs/tailwindcss"],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,8 @@
 <template>
-  <div>
-    <button type="button" name="button" @click="getMsg">
-      RailsからAPIを取得する
-    </button>
-    <div v-for="(msg, i) in msgs" :key="i">
-      {{ msg }}
-    </div>
+  <div class="w-9/10 max-w-custom mx-auto my-20 rounded-sm shadow-custom bg-white text-center py-5">
+    <p>ようこそ！</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useNuxtApp } from '#app'
-
-const msgs = ref<string[]>([])
-const { $axios } = useNuxtApp()
-
-const getMsg = async () => {
-  try {
-    const res = await $axios.get('/api/v1/hello')
-    msgs.value.push(res.data)
-  } catch (error) {
-    console.error(error)
-  }
-}
 </script>
