cfd2854978e7ad96224e0a593650f0f56d0a2114
takaki <takaki55730317@gmail.com>
2024-06-20 14:39:01 +0900

    Add LoginForm component and update index page layout
    - Created  component with email and password fields
    - Applied responsive design using TailwindCSS
    - Integrated  into  and adjusted layout for better presentation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="w-9/10 max-w-custom mx-auto my-20 rounded-sm shadow-custom bg-white text-center py-5">
+  <div class="w-full sm:w-9/10 md:max-w-custom mx-auto my-20 rounded-sm shadow-custom bg-white text-center py-5">
     <p>ようこそ！</p>
+    <div class="mt-4 flex justify-center">
+      <LoginForm />
+    </div>
   </div>
 </template>
 
