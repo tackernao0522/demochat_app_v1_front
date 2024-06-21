918d24ff3ddbd2391ce5a852b6415530d3033cda
takaki <takaki55730317@gmail.com>
2024-06-21 16:15:58 +0900

    Refactor LoginForm and SignupForm to use reusable components

---
+++ components/FormField.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input :type="type" :placeholder="placeholder" :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            class="block w-full p-2 border border-gray-300 rounded" />
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    type: {
+        type: String,
+        default: 'text'
+    },
+    placeholder: {
+        type: String,
+        default: ''
+    },
+    modelValue: {
+        type: String,
+        default: ''
+    }
+})
+
+defineEmits(['update:modelValue'])
+</script>
--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -2,10 +2,8 @@
     <div class="w-full sm:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto text-center">
         <h2 class="text-xl font-bold mb-4">ログイン</h2>
         <form @submit.prevent="login" class="space-y-4">
-            <input type="email" required placeholder="メールアドレス" v-model="email"
-                class="block w-full p-2 border border-gray-300 rounded" />
-            <input type="password" required placeholder="パスワード" v-model="password"
-                class="block w-full p-2 border border-gray-300 rounded" />
+            <FormField type="email" placeholder="メールアドレス" v-model="email" />
+            <FormField type="password" placeholder="パスワード" v-model="password" />
             <button
                 class="bg-bodyBg text-white font-bold border-0 rounded px-5 py-2 cursor-pointer hover:bg-opacity-90 transition">
                 ログインする
@@ -23,6 +21,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useNuxtApp } from '#app'
+import FormField from './FormField.vue'
 
 const email = ref('')
 const password = ref('')
