1c0d2b5434b4917685782186c5e0d58bf06a83aa
takaki <takaki55730317@gmail.com>
2024-07-17 14:44:51 +0900

    modify basic-auth.glaobal.ts

--- middleware/basic-auth.global.ts
+++ middleware/basic-auth.global.ts
@@ -1,50 +1,41 @@
-import {
-  defineNuxtRouteMiddleware,
-  useRuntimeConfig,
-  useRequestHeaders,
-} from "#app";
-import { sendRedirect } from "h3";
+import { defineNuxtRouteMiddleware, useRuntimeConfig } from "#app";
+import { useRequestHeaders } from "h3";
 
 export default defineNuxtRouteMiddleware((to, from) => {
   if (process.server && process.env.NODE_ENV === "production") {
     const config = useRuntimeConfig();
-    console.log("Runtime config:", config); // デバッグ用ログ
 
     const auth = useRequestHeaders(["authorization"]);
-    console.log("Authorization header:", auth.authorization); // デバッグ用ログ
 
     if (!auth.authorization) {
-      console.log("No authorization header found"); // デバッグ用ログ
-      return sendRedirect(to.path, {
+      return {
         status: 401,
         headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-      });
+      };
     }
 
     const [, base64Credentials] = auth.authorization.split(" ");
-    const credentials = atob(base64Credentials);
+    const credentials = Buffer.from(base64Credentials, "base64").toString(
+      "utf-8"
+    );
     const [username, password] = credentials.split(":");
-    console.log("Received credentials:", username, password); // デバッグ用ログ
 
     if (!config.BASIC_AUTH_USERNAME || !config.BASIC_AUTH_PASSWORD) {
-      console.error("Basic auth credentials not set in environment"); // エラーログ
-      return sendRedirect(to.path, {
+      console.error("Basic auth credentials not set in environment");
+      return {
         status: 500,
         statusMessage: "Server configuration error",
-      });
+      };
     }
 
     if (
       username !== config.BASIC_AUTH_USERNAME ||
       password !== config.BASIC_AUTH_PASSWORD
     ) {
-      console.log("Invalid credentials"); // デバッグ用ログ
-      return sendRedirect(to.path, {
+      return {
         status: 401,
         headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-      });
+      };
     }
-
-    console.log("Authentication successful"); // デバッグ用ログ
   }
 });
